abd9a4e81ba4af445349fca02091240fb9be0343
harshx3 <harshmakwana686@gmail.com>
2025-01-23 16:40:59 -0600

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Harsh Makwana on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
